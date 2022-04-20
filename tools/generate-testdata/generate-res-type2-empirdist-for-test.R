77ba643cba959b873cd2cf5df640bcdf5bf578ed
fshin3 <fshin3@gmail.com>
2022-04-20 14:44:29 +0900

    add test data and generate code

--- tools/generate-testdata/generate-res-type2-empirdist-for-test.R
+++ tools/generate-testdata/generate-res-type2-empirdist-for-test.R
@@ -13,7 +13,7 @@ AAV <- 0.4
 tune.par <- c(0.5,0.5,0.4)
 beta <- 1.0
 
-# empir.distのsimple.dist or not
+# empir.distのsimple.empir or not
 simple <- FALSE
 
 delta1 <- tune.par[1]   # velocity to go to BT
@@ -28,10 +28,11 @@ n<-nrow(data_aka)
 l.catch <- length(data_aka$catch)
 mean.catch <- mean(data_aka$catch[(l.catch-n.catch+1):l.catch],na.rm = TRUE)
 cpue <- data_aka$cpue[!is.na(data_aka$cpue)]
+
+# empir.dist=T, simple.empir=F
 cum.cpue.empir <- ecdf(cpue) # cumulative empirical distribution
 cD <- cum.cpue.empir(cpue[n])
 D <- cum.cpue.empir(cpue)
-icum.cpue <- function(x) as.numeric(quantile(cpue,x))
 
 ifelse(delta3 > 0, AAV <- aav.f(data_aka$cpue), AAV <- 0)
 
@@ -78,3 +79,27 @@ aka_abc2_empir <- list(BRP=BRP,Obs_BRP=Obs_BRP,Current_Status=Current_Status,AAV
 
 save(aka_abc2_empir,file="./inst/extdata/res_aka_abc2_empir.rda")
 
+# empir.distのsimple.empir=Tの場合
+
+cD <- (cpue[n]-min(cpue))/(max(cpue)-min(cpue))
+D <- (cpue-min(cpue))/(max(cpue)-min(cpue))
+
+ifelse(delta3 > 0, AAV <- aav.f(data_aka$cpue), AAV <- 0)
+
+if(cD <= BB) alpha <- 0
+if(BB < cD & cD < BL){
+  k <- delta1 + delta2* exp(delta3*log(AAV^2+1)) * (BL-cD)/(cD-BB)
+  alpha <- exp(k*(cD-BT))
+}
+if(cD >= BL) alpha <- exp(delta1*(cD-BT))
+alpha<-alpha*beta
+
+ABC <- mean.catch * alpha
+Obs_BRP <- c(icum.cpue(BT), icum.cpue(BL), icum.cpue(BB))
+names(BRP) <- names(Obs_BRP) <- c("Target","Limit","Ban")
+Current_Status <- c(D[n],cpue[n])
+names(Current_Status) <- c("Level","CPUE")
+
+aka_abc2_empir_simple <- list(BRP=BRP,Obs_BRP=Obs_BRP,Current_Status=Current_Status,AAV=AAV,tune.par=tune.par,ABC=ABC)
+
+save(aka_abc2_empir_simple,file="./inst/extdata/res_aka_abc2_empir_simple.rda")
---
+++ tools/generate-testdata/generate-res-type2-smoothcpue-for-test.R
@@ -0,0 +1,64 @@
+source("./tools/generate-testdata/abc_t23.r")
+
+#data_akaをつかう。
+data("data_aka")
+#head(data_aka)
+
+# セッティングパラメータ
+n.catch <- 5
+BT <- 0.8
+PL <- 0.7
+PB <- 0
+AAV <- 0.4
+tune.par <- c(0.5,0.5,0.4)
+beta <- 1.0
+
+# CPUE平滑化の移動平均年数
+n.cpue <- 3
+
+delta1 <- tune.par[1]   # velocity to go to BT
+delta2 <- tune.par[2]   # correction factor when D <= BL
+delta3 <- tune.par[3]   # tuning parameter for updating BT
+BT <- BT      # Btarget
+BL <- PL*BT      # Blimit
+BB <- PB*BT      # Bban
+BRP <- c(BT, BL, BB)
+
+n<-nrow(data_aka)
+l.catch <- length(data_aka$catch)
+mean.catch <- mean(data_aka$catch[(l.catch-n.catch+1):l.catch],na.rm = TRUE)
+cpue <- data_aka$cpue[!is.na(data_aka$cpue)]
+l.cpue <- length(cpue)
+mean.cpue <- mean(cpue[(l.cpue-n.cpue+1):l.cpue],na.rm = TRUE)
+
+cD <- pnorm(mean.cpue,mean(cpue),sd(cpue))
+D <- pnorm(scale(cpue),0,1)
+mD <- attributes(D)$'scaled:center'
+sD <- attributes(D)$'scaled:scale'
+icum.cpue <- function(x) sD*qnorm(x,0,1)+mD   # inverse function from D to CPUE
+
+ifelse(delta3 > 0, AAV <- aav.f(data_aka$cpue), AAV <- 0)
+
+if(cD <= BB) alpha <- 0
+if(BB < cD & cD < BL){
+  k <- delta1 + delta2* exp(delta3*log(AAV^2+1)) * (BL-cD)/(cD-BB)
+  alpha <- exp(k*(cD-BT))
+}
+if(cD >= BL) alpha <- exp(delta1*(cD-BT))
+alpha<-alpha*beta
+
+ABC <- mean.catch * alpha
+Obs_BRP <- c(icum.cpue(BT), icum.cpue(BL), icum.cpue(BB))
+names(BRP) <- names(Obs_BRP) <- c("Target","Limit","Ban")
+Recent_Status <- c(cD,mean.cpue)
+names(Recent_Status) <- c("Level","CPUE")
+
+aka_abc2_sm3cpue<- list(BRP=BRP,Obs_BRP=Obs_BRP,Current_Status=Recent_Status,AAV=AAV,tune.par=tune.par,ABC=ABC)
+
+save(aka_abc2_sm3cpue,file="./inst/extdata/res_aka_abc2_sm3cpue.rda")
+
+# CPUE時系列全体を平滑化
+smoothed.cpue <- c()
+for(i in n.cpue:l.cpue){
+  smoothed.cpue <- cbind(smoothed.cpue,mean(cpue[(i-n.cpue+1):i],na.rm = TRUE))
+}
